521a0360ba8178fe2eb10593154ddd07eafe2b39
Cameron Chamberlain <hi@cameronchamberlain.com>
2012-08-30 15:49:36 +1000

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c809dfff85fec4ea6dbe332d851c0645eaff2402b4814fb8db8f82d0357141647a998c498bf40e220f06dbddd7e55d4a74e2ed9a1f0bbe6ac7dbaa411f5c1d9c'
